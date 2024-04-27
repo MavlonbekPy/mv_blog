15c53572d3f63db3c45d258bde5da1c36016fc81
Mavlonbek <153426906+era1iyevv@users.noreply.github.com>
2024-04-27 18:19:40 +0500

    main things sorted

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,11 @@ import requests
 
 def home_view(request):
     return render(request, 'index.html')
+
+
+def about_view(request):
+    return render(request, 'about.html')
+
+
+def blog_view(request):
+    return render(request, 'blog.html')
