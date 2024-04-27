b8ab21c1e8d188b613f814b62dfe1949b0d622d6
Mavlonbek <153426906+era1iyevv@users.noreply.github.com>
2024-04-27 18:44:12 +0500

    post model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    is_published = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def admin_photo(self):
+        return mark_safe('<img src="{}" width="70" />'.format(self.image.url))
+
+    admin_photo.short_description = 'Image'
+    admin_photo.allow_tags = True
+
+    def __str__(self):
+        return self.titl
