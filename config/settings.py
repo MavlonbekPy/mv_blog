4f7657305864903f7bcd8b8e9fb95a4f1090bd4d
Mavlonbek <153426906+MavlonbekPy@users.noreply.github.com>
2024-06-21 15:22:24 +0500

    Update settings.py
    enabled allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-4h2zh0!9&#h+7qx5@4cf$hxex0newkbo7pwl3pys@h^g9ae@1!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
