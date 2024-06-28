af373aac485952ed000e58439c667e2dc001e819
Mavlonbek <153426906+MavlonbekPy@users.noreply.github.com>
2024-06-28 18:15:50 +0500

    Update settings.py
    databes changed

--- config/settings.py
+++ config/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mavlon_blog",
+        "USER": "doadmin",
+        "PASSWORD": "AVNS_feJOFOu7KkwbdfMc3JX",
+        "HOST": "do-db-do-user-16947834-0.c.db.ondigitalocean.com",
+        "PORT": "25060",
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
